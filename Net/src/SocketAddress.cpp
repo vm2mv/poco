94eb76eba5e4fbd82b91fa9a8f29832390eae41d
Vasil Mikulich <vm@exlade.com>
2023-10-17 18:48:22 +0300

    * Net: do DNS::hostByNameSocket with Address::init DNS_HINT_NONE

--- Net/src/SocketAddress.cpp
+++ Net/src/SocketAddress.cpp
@@ -279,7 +279,7 @@ void SocketAddress::init(const std::string& hostAddress, Poco::UInt16 portNumber
 	}
 	else
 	{
-		HostEntry he = DNS::hostByName(hostAddress);
+		HostEntry he = DNS::hostByName(hostAddress, 0);
 		HostEntry::AddressList addresses = he.addresses();
 		if (addresses.size() > 0)
 		{
@@ -304,7 +304,7 @@ void SocketAddress::init(Family fam, const std::string& hostAddress, Poco::UInt1
 	}
 	else
 	{
-		HostEntry he = DNS::hostByName(hostAddress);
+		HostEntry he = DNS::hostByName(hostAddress, 0);
 		HostEntry::AddressList addresses = he.addresses();
 		if (addresses.size() > 0)
 		{
