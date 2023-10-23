1379bac750d329621f039a3be529bacefc12b0d8
Vasil Mikulich <vm@exlade.com>
2023-10-23 14:09:07 +0300

    Net: disable POCO_HAVE_FD_EPOLL on Windows, see https://github.com/pocoproject/poco/issues/3796

--- Net/include/Poco/Net/Net.h
+++ Net/include/Poco/Net/Net.h
@@ -134,7 +134,9 @@ POCO_NET_FORCE_SYMBOL(pocoNetworkInitializer)
 #endif
 
 
-#if (POCO_OS == POCO_OS_LINUX) || (POCO_OS == POCO_OS_WINDOWS_NT) || (POCO_OS == POCO_OS_ANDROID)
+// #if (POCO_OS == POCO_OS_LINUX) || (POCO_OS == POCO_OS_WINDOWS_NT) || (POCO_OS == POCO_OS_ANDROID)
+// disable POCO_HAVE_FD_EPOLL on Windows, see https://github.com/pocoproject/poco/issues/3796
+#if (POCO_OS == POCO_OS_LINUX)
 	#define POCO_HAVE_FD_EPOLL 1
 #endif
 
